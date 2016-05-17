e8e75186321eabfb0b89ffe35308ab2ec9ff86cc
Joshua Masland-Sarani <j.masland@gmail.com>
2016-05-17 14:00:59 -0400

    resume tweaks

--- js/view.js
+++ js/view.js
@@ -12,7 +12,7 @@ B.view = (function() {
   };
 
   var _clear = function() {
-    ctx.fillStyle = "#ffffff";
+    ctx.fillStyle = "rgba(69, 83, 114, 1.0)";
     ctx.clearRect( 0, 0, c.width, c.height );
     ctx.fillRect( 0, 0, c.width, c.height );
   }
@@ -20,7 +20,7 @@ B.view = (function() {
   var _render = function( bubbles ) {
     for ( var i = 0; i < bubbles.length; i++ ) {
       b = bubbles[i];
-      ctx.fillStyle = "#455372";
+      ctx.fillStyle = "rgba(250, 250, 250, 0.75)";
       ctx.beginPath();
       ctx.arc( b.x, b.y, b.size, 0, 2*Math.PI );
       ctx.fill();
